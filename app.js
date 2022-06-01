ea450a87fb9fd9f46f89851b9229188886639033
Maycon Mendes <mayconsouzamendes@gmail.com>
2022-06-01 10:33:24 -0300

    Adding Tempo
    Adding tempo;
    * Html;
    * Css;
    * Javascript

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ class DrumKit {
         this.isPlaying = null;
         this.selects = document.querySelectorAll('select');
         this.muteBtns = document.querySelectorAll('.mute')
+        this.tempoSlider = document.querySelector('.tempo-slider');
     }
 
     activePad() {
@@ -122,6 +123,21 @@ class DrumKit {
             }
         }
     }
+
+    changeTempo(e) {
+        const tempoText = document.querySelector('.tempo-nr');
+        tempoText.innerText = e.target.value;
+    }
+
+    updateTempo(e) {
+        this.bpm = e.target.value;
+        clearInterval(this.isPLaying);
+        this.isPLaying = null;
+        const playBtn = document.querySelector('.play');
+        if(playBtn.classList.contains('active')){
+            this.start();
+        }
+    }
 }
 
 const drumKit = new DrumKit();
@@ -150,4 +166,12 @@ drumKit.muteBtns.forEach((btn) => {
     btn.addEventListener('click', function(e) {
         drumKit.mute(e);
     })
+})
+
+drumKit.tempoSlider.addEventListener('input', function(e) {
+    drumKit.changeTempo(e);
+})
+
+drumKit.tempoSlider.addEventListener('change', function(e) {
+    drumKit.updateTempo(e);
 })
